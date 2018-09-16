d776beaae4c13463602a04a22173a8499c6e8537
cmllr <fury224@googlemail.com>
2018-09-16 13:47:41 +0200

    Added dockerfile modifications

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ WORKDIR /code
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt
 ADD . /code/
-CMD [ "python", "./manage.py runserver 0.0.0.0:8000" ]
+expose 8000
+CMD [ "python", "./manage.py", "runserver", "0.0.0.0:8000" ]
--- pitlane/settings.py
+++ pitlane/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'oof2syr2*ayurpd#6l9!cvonck442n&g(td-jab!ths0ekbonn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+# for test purposes..
+ALLOWED_HOSTS = ["*", "127.0.0.1", "localhost"]^M
+
 
 
 # Application definition
