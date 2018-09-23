3e8965f32018b9495829f75b5a47e169fc02feec
cmllr <fury224@googlemail.com>
2018-09-23 13:38:02 +0200

    Added time tag, changed race display

---
+++ frontend/templatetags/frontend_extras.py
@@ -0,0 +1,13 @@
+
+from datetime import datetime, timedelta
+from django import template
+
+register = template.Library()
+@register.filter
+def time(seconds) -> str:
+  if seconds == "-" or seconds == "":
+    return ""
+  seconds = float(seconds)
+  hours, remainder = divmod(seconds, 3600)
+  minutes, seconds = divmod(remainder, 60)
+  return'{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds))
