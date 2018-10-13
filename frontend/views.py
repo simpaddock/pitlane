73421e14423b65a690fa74d91c394b66a59aef36
cmllr <fury224@googlemail.com>
2018-10-14 00:17:25 +0200

    Added 404 management for /calendar/

--- frontend/views.py
+++ frontend/views.py
@@ -96,7 +96,7 @@ def renderWithCommonData(request, template, context):
   context["baseLayout"] = 'frontend/'+  LEAGUECONFIG["theme"] + '/layout.html'
   return render(request, template, context)
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_index(request):
   from datetime import datetime
   articles = NewsArticle.objects.all().order_by("date")
@@ -123,7 +123,7 @@ def get_index(request):
     "newsArticles": newsArticles
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_news(request):
   articles = NewsArticle.objects.all().order_by("date")
   paginator = Paginator(articles, 5)
@@ -133,7 +133,7 @@ def get_news(request):
     "isInSingleMode": False
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_about(request):
   name = LEAGUECONFIG["name"]
   logo = LEAGUECONFIG["logo"]
@@ -158,21 +158,21 @@ def get_about(request):
     "textBlocks": textBlocks
   })
 
-#@cache_page(60 * 15)  
+@cache_page(60 * 15)  
 def get_rules(request):
   return renderWithCommonData(request, 'frontend/rules.html', {
     "rules": TextBlock.objects.filter(season__isRunning=True, context='rule')
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_privacy(request):
   return renderWithCommonData(request, 'frontend/privacy.html', {})
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_imprint(request):
   return renderWithCommonData(request, 'frontend/imprint.html', {})
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_SingleNews(request, id:int):
   articles = NewsArticle.objects.all().filter(pk=id)
   paginator = Paginator(articles, 5)
@@ -195,7 +195,7 @@ class JSONEncoder(DjangoJSONEncoder):
     def default(self, o):
         return str(o)
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_seasonList(request):
   seasonList = Season.objects.all()
   races = {}
@@ -360,7 +360,7 @@ def getDriversStandings(id: int):
   viewList = list(viewList.values())
   return sorted(viewList, key=lambda tup: tup["sum"], reverse=True)
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_raceDetail(request, id: int):
   race = Race.objects.all().filter(pk=id).get()
   resultList = getRaceResult(race.id)
@@ -381,7 +381,7 @@ def get_raceDetail(request, id: int):
     "commentatorInfo": raceResult.commentatorInfo 
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_seasonStandingsTeams(request, id: int):
   racesRaw = Race.objects.filter(season_id=id).order_by('startDate') 
   resultList = getTeamStandings(id)
@@ -397,7 +397,7 @@ def get_seasonStandingsTeams(request, id: int):
     "season": season
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_raceBanner(request, id: int):
   race = Race.objects.filter(pk=id).get()
   schedule = TextBlock.objects.filter(title="Race Schedule", season_id=race.season.pk).first()
@@ -440,7 +440,7 @@ def get_raceBanner(request, id: int):
   img.save(response, "PNG")
   return response
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_seasonStandingsDrivers(request, id: int):
   resultList = getDriversStandings(id)
   racesRaw = Race.objects.filter(season_id=id).order_by('startDate') 
@@ -483,13 +483,15 @@ def incidentReport(request):
   return renderWithCommonData(request, 'frontend/incident.html', {
     "form": form
   })
-
+@cache_page(60 * 15)
 def get_iCalender(request, id: int):
   tz = pytz.timezone(LEAGUECONFIG["timezone"])
   calendar = Calendar()
   calendar.add('prodid', '-//SimPaddock//')
   calendar.add('version', '2.0')
   season = Season.objects.filter(pk=id).first()
+  if season is None:
+    raise Http404()
   races = Race.objects.filter(season_id=season.id)
   for race in races:
     event = Event()
