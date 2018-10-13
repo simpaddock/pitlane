b6eed270631e5ffe06d53bcd37739c2964380060
cmllr <fury224@googlemail.com>
2018-10-13 17:37:43 +0200

    test db update

--- frontend/views.py
+++ frontend/views.py
@@ -94,7 +94,7 @@ def renderWithCommonData(request, template, context):
   context["baseLayout"] = 'frontend/'+  LEAGUECONFIG["theme"] + '/layout.html'
   return render(request, template, context)
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_index(request):
   from datetime import datetime
   articles = NewsArticle.objects.all().order_by("date")
@@ -121,7 +121,7 @@ def get_index(request):
     "newsArticles": newsArticles
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_news(request):
   articles = NewsArticle.objects.all().order_by("date")
   paginator = Paginator(articles, 5)
@@ -130,7 +130,7 @@ def get_news(request):
     "articles": paginator.get_page(page)
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_about(request):
   name = LEAGUECONFIG["name"]
   logo = LEAGUECONFIG["logo"]
@@ -155,21 +155,21 @@ def get_about(request):
     "textBlocks": textBlocks
   })
 
-#@cache_page(60 * 15)  
+@cache_page(60 * 15)  
 def get_rules(request):
   return renderWithCommonData(request, 'frontend/rules.html', {
     "rules": TextBlock.objects.filter(season__isRunning=True, context='rule')
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_privacy(request):
   return renderWithCommonData(request, 'frontend/privacy.html', {})
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_imprint(request):
   return renderWithCommonData(request, 'frontend/imprint.html', {})
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_SingleNews(request, id:int):
   articles = NewsArticle.objects.all().filter(pk=id)
   paginator = Paginator(articles, 5)
@@ -191,7 +191,7 @@ class JSONEncoder(DjangoJSONEncoder):
     def default(self, o):
         return str(o)
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_seasonList(request):
   seasonList = Season.objects.all()
   races = {}
@@ -356,7 +356,7 @@ def getDriversStandings(id: int):
   viewList = list(viewList.values())
   return sorted(viewList, key=lambda tup: tup["sum"], reverse=True)
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_raceDetail(request, id: int):
   race = Race.objects.all().filter(pk=id).get()
   resultList = getRaceResult(race.id)
@@ -377,7 +377,7 @@ def get_raceDetail(request, id: int):
     "commentatorInfo": raceResult.commentatorInfo 
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_seasonStandingsTeams(request, id: int):
   racesRaw = Race.objects.filter(season_id=id).order_by('startDate') 
   resultList = getTeamStandings(id)
@@ -393,7 +393,7 @@ def get_seasonStandingsTeams(request, id: int):
     "season": season
   })
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_raceBanner(request, id: int):
   race = Race.objects.filter(pk=id).get()
   schedule = TextBlock.objects.filter(title="Race Schedule", season_id=race.season.pk).first()
@@ -436,7 +436,7 @@ def get_raceBanner(request, id: int):
   img.save(response, "PNG")
   return response
 
-#@cache_page(60 * 15)
+@cache_page(60 * 15)
 def get_seasonStandingsDrivers(request, id: int):
   resultList = getDriversStandings(id)
   racesRaw = Race.objects.filter(season_id=id).order_by('startDate') 
