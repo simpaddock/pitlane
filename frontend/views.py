d4a7789e68093f8c99ceb42564835ff005e6e72e
cmllr <fury224@googlemail.com>
2018-11-17 20:44:16 +0100

    Fruther frontend changes for better mobile display

--- frontend/views.py
+++ frontend/views.py
@@ -114,7 +114,7 @@ def get_index(request):
     "now": now
   })
 
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_news(request):
   articles = NewsArticle.objects.all().order_by("-date")
   paginator = Paginator(articles, 50)
@@ -123,7 +123,7 @@ def get_news(request):
     "articles": paginator.get_page(page)
   })
 
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_about(request):
   name = LEAGUECONFIG["name"]
   logo = LEAGUECONFIG["logo"]
@@ -134,21 +134,21 @@ def get_about(request):
     "textBlocks": textBlocks
   })
 
-@cache_page(60 * 15)  
+#@cache_page(60 * 15)  
 def get_rules(request):
   return renderWithCommonData(request, 'frontend/rules.html', {
     "rules": TextBlock.objects.filter(season__isRunning=True, context='rule')
   })
 
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_privacy(request):
   return renderWithCommonData(request, 'frontend/privacy.html', {})
 
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_imprint(request):
   return renderWithCommonData(request, 'frontend/imprint.html', {})
 
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_SingleNews(request, id:int):
   articles = list(NewsArticle.objects.all().filter(pk=id))
   paginator = Paginator(articles, 5)
@@ -211,7 +211,7 @@ class JSONEncoder(DjangoJSONEncoder):
     def default(self, o):
         return str(o)
 
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_seasonList(request):
   seasonList = Season.objects.all().order_by("-isRunning")
   races = {}
@@ -387,7 +387,7 @@ def get_incidents(request, id: int):
     "incidents": incidents,
     "incidentsPendingCount": incidentsPendingCount
   })
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_raceDetail(request, id: int):
   race = Race.objects.all().filter(pk=id).get()
   resultList = getRaceResult(race.id)
@@ -412,7 +412,7 @@ def get_raceDetail(request, id: int):
 def isSeasonFinished(season):
   races = Race.objects.filter(season=season).order_by('startDate') 
   return not season.isRunning or season.round == races.count()
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_seasonStandingsTeams(request, id: int):
   racesRaw = Race.objects.filter(season_id=id).order_by('startDate') 
   resultList = getTeamStandings(id)
@@ -430,7 +430,7 @@ def get_seasonStandingsTeams(request, id: int):
     "textBlocks": TextBlock.objects.filter(season_id=id, context="tstandings")
   })
 
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_raceBanner(request, id: int):
   race = Race.objects.filter(pk=id).get()
   schedule = TextBlock.objects.filter(title="Race Schedule", season_id=race.season.pk).first()
@@ -473,7 +473,7 @@ def get_raceBanner(request, id: int):
   img.save(response, "PNG")
   return response
 
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_seasonStandingsDrivers(request, id: int):
   resultList = getDriversStandings(id)
   racesRaw = Race.objects.filter(season_id=id).order_by('startDate') 
@@ -561,7 +561,7 @@ def incidentReport(request):
   return renderWithCommonData(request, 'frontend/incident.html', {
     "form": form
   })
-@cache_page(60 * 15)
+#@cache_page(60 * 15)
 def get_iCalender(request, id: int):
   tz = pytz.timezone(LEAGUECONFIG["timezone"])
   calendar = Calendar()
