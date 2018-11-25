b902de5db3cc9d2406e8472f0421f7bf2ffa9173
cmllr <fury224@googlemail.com>
2018-11-25 09:29:09 +0100

    Applied new paths to gulpfile

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'), sass = require('gulp-sass');
 const theme = "scratch"
 gulp.task('css', function () {
 	// take files with extension .scss from /scss folder
-	return gulp.src('./templates/frontend/scratch/scss/'+theme+'/style.scss')
+	return gulp.src('./templates/frontend/'+theme+'/scss/style.scss')
 		.pipe(sass({}).on('error', sass.logError)
 		)
 		// return into css folder
@@ -12,7 +12,7 @@ gulp.task('css', function () {
 
 gulp.task('default', ['css'], function() {
 	// watch for CSS changes
-	gulp.watch('./templates/frontend/scratch/scss/'+theme+'/*.scss', function() {
+	gulp.watch('./templates/frontend/'+theme+'/scss/*.scss', function() {
 	
 		 // run styles upon changes
 		 gulp.run('css');
