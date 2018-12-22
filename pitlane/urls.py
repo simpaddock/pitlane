20f290877b175e9177b7f7039e39cd45ee89109b
cmllr <fury224@googlemail.com>
2018-12-22 13:33:52 +0100

    Added config variables and maintenance mode

--- pitlane/urls.py
+++ pitlane/urls.py
@@ -21,34 +21,45 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from frontend.feeds import LatestEntriesFeed
+from pitlane.settings import  LEAGUECONFIG
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('robots.txt', views.get_robots),
-    path('', views.get_index),
-    path('seasons/', views.get_seasonList),
-    path('news/', views.get_news),
-    path('about/', views.get_about),
-    path('privacy/', views.get_privacy),
-    path('imprint/', views.get_imprint),
-    path('calendar/<int:id>/', views.get_iCalender),
-    path('news/<int:id>/', views.get_SingleNews),
-    path('incidents/<int:id>/', views.get_incidents),
-    path('signup/', views.signUp),
-    path('driveroftheday/<int:id>/', views.driverOfTheDayVote),
-    path('privacyaccept/', views.privacyAccept), # is only enabled when using static signup
-    path('signupstatus/', views.signUpStatus),
-    path('rules/', views.get_rules),
-    path('incidentreport/', views.incidentReport),
-    path('seasons/<int:id>/drivers/', views.get_seasonStandingsDrivers),
-    path('seasons/<int:id>/teams/', views.get_seasonStandingsTeams),
-    path('races/<int:id>/', views.get_raceDetail),
-    path('driver/<int:id>/', views.getDriverStats),
-    path('embed/video/youtube/<argument>/', views.embedYoutube),
-    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    path('feed/', LatestEntriesFeed())
-] 
+if LEAGUECONFIG["maintenance"]:
+    urlpatterns = [
+        url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+        url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+        url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+        path('feed/', LatestEntriesFeed()),
+        path('admin/', admin.site.urls),
+        path('robots.txt', views.get_robots),
+        url(r'^.*', views.maintenance),
+    ]
+else:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('robots.txt', views.get_robots),
+        path('', views.get_index),
+        path('seasons/', views.get_seasonList),
+        path('news/', views.get_news),
+        path('about/', views.get_about),
+        path('privacy/', views.get_privacy),
+        path('imprint/', views.get_imprint),
+        path('calendar/<int:id>/', views.get_iCalender),
+        path('news/<int:id>/', views.get_SingleNews),
+        path('incidents/<int:id>/', views.get_incidents),
+        path('signup/', views.signUp),
+        path('driveroftheday/<int:id>/', views.driverOfTheDayVote),
+        path('privacyaccept/', views.privacyAccept), # is only enabled when using static signup
+        path('signupstatus/', views.signUpStatus),
+        path('rules/', views.get_rules),
+        path('incidentreport/', views.incidentReport),
+        path('seasons/<int:id>/drivers/', views.get_seasonStandingsDrivers),
+        path('seasons/<int:id>/teams/', views.get_seasonStandingsTeams),
+        path('races/<int:id>/', views.get_raceDetail),
+        path('driver/<int:id>/', views.getDriverStats),
+        path('embed/video/youtube/<argument>/', views.embedYoutube),
+        url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+        url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+        url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+        path('feed/', LatestEntriesFeed())
+    ] 
 
